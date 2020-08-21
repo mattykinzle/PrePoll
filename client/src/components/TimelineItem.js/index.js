65fdac1af4844003f673109db9946d3355b47cc9
Matthew Kinzle <matt.kinzle@gmail.com>
2020-08-21 10:29:26 -0500

    Links, History

--- client/src/components/TimelineItem.js/index.js
+++ client/src/components/TimelineItem.js/index.js
@@ -5,9 +5,9 @@ import "./style.css";
 const TimelineItem = ({ data }) => (
     <div className="timeline-item">
         <div className="timeline-item-content">
-            <p className="date">{data.date}</p>
-            <h3 className="header">{data.header}</h3>
-            <p className="bodyText">{data.text}</p>
+            <p className="date timeline-text">{data.date}</p>
+            <h3 className="header timeline-text">{data.header}</h3>
+            <p className="bodyText timeline-text">{data.text}</p>
             <span className="circle"></span>
         </div>
     </div>
--- client/src/data.js
+++ client/src/data.js
@@ -1,112 +1,81 @@
 const data = [
     {
-        header: "Only people who own land can vote ",
+        header: "Only people who own land can vote.",
         text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        date: '1776'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "George Washington elected president.",
+        text: "Only 6% of the population can vote.",
+        date: '1789'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "Activists for ending slavery and women’s rights join together.",
+        text: "Women’s rights convention held in Seneca Falls, NY. Frederick Douglass, a newspaper editor and former slave, attends the event and gives a speech supporting universal voting rights. His speech helps convince the convention to adopt a resolution calling for voting rights for women.",
+        date: '1848'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "Vote expanded to all white men.",
+        text: "North Carolina is the last state to remove property ownership as a requirement to vote.",
+        date: '1856'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "Former slaves granted citizenship.",
+        text: "14th Amendment to the U.S. Constitution passed. Citizenship is defined and granted to former slaves. Voters, however, are explicitly defined as male. Although the amendment forbids states from denying any rights of citizenship, voting regulation is still left in the hands of the states.",
+        date: '1868'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "Vote cannot be denied because of race, explicitly.",
+        text: "15th Amendment passed. It states that the right to vote cannot be denied by the federal or state governments based on race.",
+        date: '1870'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "Women try to vote.",
+        text: "Susan B. Anthony is arrested and brought to trial in Rochester, New York, for attempting to vote in the presidential election. At the same time, Sojourner Truth, a former slave and advocate for justice and equality, appears at a polling booth in Grand Rapids, Michigan, demanding a ballot but she is turned away.",
+        date: '1872'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "Indigenous people cannot vote.",
+        text: "The Supreme Court rules that Native Americans are not citizens as defined by the 14th Amendment and, thus, cannot vote.",
+        date: '1876'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "Wyoming admitted to statehood and becomes first state to legislate voting for women in its constitution.",
+        date: '1890'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "Women lead voting rights marches through New York and Washington, D.C.",
+        date: '1912-1913'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "Right to vote extended to women.",
+        text: "19th Amendment passed, giving women right to vote in both state and federal elections.",
+        date: '1920'
     },
     {
-        header: "Only people who own land can vote ",
-        text: "Declaration of Independence signed. Right to vote during the Colonial and Revolutionary periods is restricted to property owners—most of whom are white male Protestants over the age of 21.",
-        date: '10/17/1983',
-        category: {
-            tag: 'sign that bill',
-            color: '#457b9d'
-        }
+        header: "Voting rights as civil rights.",
+        text: "Large-scale efforts in the South to register African Americans to vote are intensified. However, state officials refuse to allow African Americans to register by using voting taxes, literacy tests and violent intimidation. Among the efforts launched is Freedom Summer, where close to a thousand civil rights workers of all races and backgrounds converge on the South to support voting rights.",
+        date: '1963-1964'
     },
-
+    {
+        header: "No special tax to vote.",
+        text: "24th Amendment passed. It guarantees that the right to vote in federal elections will not be denied for failure to pay any tax.",
+        date: '1964'
+    },
+    {
+        header: "Grassroots movement forces change in law.",
+        text: "Voting Rights Act passed. It forbids states from imposing discriminatory restrictions on who can vote, and provides mechanisms for the federal government to enforce its provisions.",
+        date: '1965'
+    },
+    {
+        header: "Voting age lowered to 18.",
+        text: "26th Amendment passed, granting voting rights to 18-year-olds. The amendment is largely a result of Vietnam War-protests demanding a lowering of the voting age on the premise that people who are old enough to fight are old enough to vote.",
+        date: '1971'
+    },
+    {
+        header: "Voting materials in various languages.",
+        text: "Amendments to Voting Rights Act require that certain voting materials be printed in languages besides English so that people who do not read English can participate in the voting process.",
+        date: '1975'
+    }
 ]
 export default data;
