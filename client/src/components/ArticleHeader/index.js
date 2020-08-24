73e21654869a5a44882bed0c072bb35037d949da
Matthew Kinzle <matt.kinzle@gmail.com>
2020-08-24 16:49:13 -0500

    landing page

---
+++ client/src/components/ArticleHeader/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+
+function ArticleHeader() {
+
+    return (
+        <>
+            <Container fluid className="main">
+                <Container className="article-Header">
+                    <Row>
+                        <h1 className="art-Header">Top News on the 2020 Election</h1>
+                    </Row>
+                </Container>
+            </Container>
+        </>
+    )
+
+}
+
+export default ArticleHeader;
---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Hero(props) {
+    return (
+        <div className="hero text-center" >
+            Welcome World
+        </div>
+    );
+}
+
+export default Hero;
