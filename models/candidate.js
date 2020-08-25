1712da39d7137d32aa9b87ff5f367fe5d0d03212
Matt Meleen <mmeleen@utexas.edu>
2020-08-25 10:41:47 -0500

    created candidate model

---
+++ models/candidate.js
@@ -0,0 +1,21 @@
+module.exports = function (sequelize, DataTypes) {
+
+  const Candidate = sequelize.define('Candidate', {
+    candidate: DataTypes.STRING,
+    email: DataTypes.STRING,
+    party: DataTypes.STRING,
+    city: DataTypes.STRING,
+    occupation: DataTypes.STRING,
+    isIncumbent: DataTypes.STRING,
+  },
+    {
+      timestamps: false
+    });
+
+  //Associations
+  Candidate.associate = function (models) {
+    Candidate.belongsTo(models.Election);
+    Candidate.hasMany(models.Note);
+  };
+  return Candidate;
+}
