253b5d1ddb3478c77c2284512ee2b978efc53bf9
Matt Meleen <mmeleen@utexas.edu>
2020-08-25 10:40:04 -0500

    Populate Elections and Candidates models

--- db/populateElections.sql
+++ db/populateElections.sql
@@ -1,8 +1,10 @@
-UPDATE ElectionData SET county = "" WHERE county IS null;
+USE Project3Dev;
+
+-- UPDATE ElectionData SET county = "" WHERE county IS null;
 
 INSERT INTO Elections (office, officeType, county)
-SELECT DISTINCT (office, officeType, county) FROM ElectionData;
+SELECT DISTINCT ElectionData.office, ElectionData.officeType, ElectionData.county FROM ElectionData;
 
 INSERT INTO Candidates (ElectionId, candidate, email, party, city, occupation, isIncumbent)
-SELECT Elections.ElectionId, ElectionData.candidate, ElectionData.email, ElectionData.party, ElectionData.city, ElectionData.occupation, ElectionData.isIncumbent
+SELECT Elections.id, ElectionData.candidate, ElectionData.email, ElectionData.party, ElectionData.city, ElectionData.occupation, ElectionData.isIncumbent
 FROM Elections INNER JOIN ElectionData ON Elections.office = ElectionData.office AND Elections.officeType = ElectionData.officeType AND Elections.county = ElectionData.county;
